bc7468b193b3628558477388c070ffb53f6deaa2
Jimmy <jparrish@citytelecoin.com>
2024-01-19 16:01:19 -0600

    update

--- main.go
+++ main.go
@@ -3,19 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	//var colors map[string]string
 
-	colors := make(map[string]string)
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
 
-	// colors := map[string]string{
-	// 	"red":   "#ff0000",
-	// 	"green": "#4bf745",
-	// 	"white": "#ffffff",
-	// }
+	printMap(colors)
+}
 
-	colors["white"] = "#ffffff"
-
-	delete(colors, "white")
-
-	fmt.Println(colors)
+func printMap(c map[string]string) {
+	for key, value := range c {
+		fmt.Println("Hex code for", key, "is", value)
+	}
 }
